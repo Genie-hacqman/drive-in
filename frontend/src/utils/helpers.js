a1cf3f10e6a5da1185dbba660835b4680a08b208
Genie-hacqman <ii813119@gmail.com>
2026-09-04 15:12:33 +0000

    added  an update

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -32,15 +32,9 @@ export const truncate = (str, length = 100) => {
   return str.length > length ? str.substring(0, length) + '...' : str;
 };
 
-
-
-
 export const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
-
-
-
 
 export const isValidEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
